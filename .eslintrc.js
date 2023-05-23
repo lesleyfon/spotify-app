9ce7b1a78bf8fb544f18f3f6a1e70d912d1646a8
lesley <lesleybanadzem2017@gmail.com>
2023-05-22 21:48:47 -0500

    Added eslint and prettier rules for formating.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,29 @@
 /** @type {import('eslint').Linter.Config} */
 module.exports = {
-  extends: ["@remix-run/eslint-config", "@remix-run/eslint-config/node"],
+	extends: [
+		"@remix-run/eslint-config",
+		"@remix-run/eslint-config/node",
+		"airbnb",
+		"plugin:prettier/recommended",
+		"plugin:react/recommended",
+		"plugin:react-hooks/recommended",
+		"plugin:jsx-a11y/recommended",
+	],
+	plugins: ["prettier", "react"],
+	parserOptions: {
+		ecmaFeatures: {
+			jsx: true,
+		},
+	},
+	rules: {
+		"prettier/prettier": ["error"],
+		"react-hooks/exhaustive-deps": "warn",
+		"react-hooks/rules-of-hooks": "error",
+		"react/jsx-filename-extension": [1, { extensions: [".js", ".jsx .ts .tsx"] }],
+		"react/jsx-indent-props": [2, 4],
+		"react/jsx-indent": [2, 4],
+		"react/jsx-one-expression-per-line": [0],
+		"react/prefer-stateless-function": [1],
+		"react/static-property-placement": [1, "property assignment"],
+	},
 };
