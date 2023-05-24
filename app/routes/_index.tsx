2d6c12d8fffddd0295d554656b4cb8d1d99cafba
lesley <lesleybanadzem2017@gmail.com>
2023-05-23 21:38:15 -0500

    created a function for fetching artist on type.

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,44 +1,54 @@
-import type { V2_MetaFunction } from "@remix-run/node";
+import type { V2_MetaFunction } from '@remix-run/node'
+import { json } from '@remix-run/node'
+import { useLoaderData } from '@remix-run/react'
+import { getArtist } from '~/utils/script.spotify'
+
+export async function loader() {
+  const artist = await getArtist('Burna Boy')
+  return json({})
+}
 
 export const meta: V2_MetaFunction = () => {
-	return [{ title: "Spotify App" }];
-};
+  return [{ title: 'Spotify App' }]
+}
 
 export default function Index() {
-	return (
-		<main className="tw-w-full">
-			<h1 className="tw-font-sans tw-text-center tw-text-8xl tw-mx-auto">Welcome!!!</h1>
-			<form className="tw-max-w-md tw-w-full tw-mx-auto tw-mt-12 ">
-				<div className="tw-relative">
-					<input
-						type="text"
-						id="floating_filled"
-						className="tw-peer tw-pointer-events-auto tw-block tw-w-full tw-appearance-none tw-border-0 tw-border-b-2 tw-border-gray-300 tw-bg-gray-50 tw-px-2.5 tw-pb-2.5 tw-pt-5 tw-text-lg tw-text-gray-900 focus:tw-border-blue-600 focus:tw-outline-none focus:tw-ring-0 dark:tw-border-gray-600 dark:tw-bg-gray-700 dark:tw-text-white dark:focus:tw-border-blue-500"
-						placeholder=" "
-					/>
+  const data = useLoaderData<typeof loader>()
 
-					<label
-						htmlFor="floating_filled"
-						className="tw-transform tw-flex tw-cursor-text tw-items-center tw-absolute tw-w-full tw-left-2.5 tw-top-4 tw-z-10 tw-origin-[0] tw--translate-y-4 tw-scale-75 tw-text-lg tw-text-gray-500 tw-duration-300 peer-placeholder-shown:tw-translate-y-0 peer-placeholder-shown:tw-scale-100 peer-focus:tw--translate-y-4 peer-focus:tw-scale-75 peer-focus:tw-text-blue-600 dark:tw-text-gray-400 peer-focus:dark:tw-text-blue-500"
-					>
-						<svg
-							xmlns="http://www.w3.org/2000/svg"
-							className="tw-top-0 tw-bottom-0 tw-w-6 tw-h-6 tw-my-auto tw-text-black-400 tw-left-3"
-							fill="none"
-							viewBox="0 0 24 24"
-							stroke="currentColor"
-						>
-							<path
-								strokeLinecap="round"
-								strokeLinejoin="round"
-								strokeWidth={2}
-								d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
-							/>
-						</svg>
-						<span>Search Artist</span>
-					</label>
-				</div>
-			</form>
-		</main>
-	);
+  return (
+    <main className="tw-w-full">
+      <h1 className="tw-font-sans tw-text-center tw-text-8xl tw-mx-auto">Welcome!!!</h1>
+      <form className="tw-max-w-md tw-w-full tw-mx-auto tw-mt-12 ">
+        <div className="tw-relative">
+          <input
+            type="text"
+            id="floating_filled"
+            className="tw-peer tw-pointer-events-auto tw-block tw-w-full tw-appearance-none tw-border-0 tw-border-b-2 tw-border-gray-300 tw-bg-gray-50 tw-px-2.5 tw-pb-2.5 tw-pt-5 tw-text-lg tw-text-gray-900 focus:tw-border-blue-600 focus:tw-outline-none focus:tw-ring-0 dark:tw-border-gray-600 dark:tw-bg-gray-700 dark:tw-text-white dark:focus:tw-border-blue-500"
+            placeholder=" "
+          />
+
+          <label
+            htmlFor="floating_filled"
+            className="tw-transform tw-flex tw-cursor-text tw-items-center tw-absolute tw-w-full tw-left-2.5 tw-top-4 tw-z-10 tw-origin-[0] tw--translate-y-4 tw-scale-75 tw-text-lg tw-text-gray-500 tw-duration-300 peer-placeholder-shown:tw-translate-y-0 peer-placeholder-shown:tw-scale-100 peer-focus:tw--translate-y-4 peer-focus:tw-scale-75 peer-focus:tw-text-blue-600 dark:tw-text-gray-400 peer-focus:dark:tw-text-blue-500"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="tw-top-0 tw-bottom-0 tw-w-6 tw-h-6 tw-my-auto tw-text-black-400 tw-left-3"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={2}
+                d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+              />
+            </svg>
+            <span>Search Artist</span>
+          </label>
+        </div>
+      </form>
+    </main>
+  )
 }
--- app/utils/script.spotify.ts
+++ app/utils/script.spotify.ts
@@ -1,82 +1,133 @@
-
-
-import SpotifyWebApi from "spotify-web-api-node";
-import dotenv from "dotenv"
+import SpotifyWebApi from 'spotify-web-api-node'
+import dotenv from 'dotenv'
 
 dotenv.config()
 
-
-
 type ENV_VARIABLE = {
-	CLIENT_ID: string;
-	CLIENT_SECRET: string;
+  CLIENT_ID: string
+  CLIENT_SECRET: string
 }
 
 const config = process.env as unknown as ENV_VARIABLE
 
+console.log({ clientId: config.CLIENT_ID, clientSecret: config.CLIENT_SECRET })
 
 const spotifyApi = new SpotifyWebApi({
-	clientId: config.CLIENT_ID,
-	clientSecret: config.CLIENT_SECRET,
-});
+  clientId: config.CLIENT_ID,
+  clientSecret: config.CLIENT_SECRET,
+})
+
 const sleep = function (time = 5000) {
-	setInterval(() => { }, time);
-};
-const trackName = new Set();
+  setInterval(() => {}, time)
+}
 
-const fetchTracks = async (artistName: string, options = { limit: 50, offset: 0 }, stopCount = 0) => {
-	if (stopCount === 1) {
-		return;
-	}
-	try {
-		const response = await spotifyApi.searchTracks(`artist:${artistName}`, options);
-		const data: any = response.body;
+const trackName = new Set()
 
-		const filteredData = data.tracks.items.filter((item: any) =>
-			item.artists.some((artist: any) => artist.name === artistName)
-		);
+const fetchTracks = async (
+  artistName: string,
+  options = { limit: 50, offset: 0 },
+  stopCount = 0
+) => {
+  if (stopCount === 1) {
+    return
+  }
+  try {
+    const response = await spotifyApi.searchTracks(`artist:${artistName}`, options)
+    const data: any = response.body
 
-		filteredData.forEach((item: any) => {
-			if (!trackName.has(item.name)) {
-				trackName.add(item.name);
-			}
-		});
+    const filteredData = data.tracks.items.filter((item: any) =>
+      item.artists.some((artist: any) => artist.name === artistName)
+    )
 
+    filteredData.forEach((item: any) => {
+      if (!trackName.has(item.name)) {
+        trackName.add(item.name)
+      }
+    })
 
-		const nextURL = data.tracks.next;
+    const nextURL = data.tracks.next
 
-		const searchParams = new URLSearchParams(nextURL);
+    const searchParams = new URLSearchParams(nextURL)
 
+    if (
+      searchParams === null ||
+      searchParams.get('limit') === null ||
+      searchParams.get('offset') === null
+    ) {
+      return
+    }
 
-		if (searchParams === null || searchParams.get("limit") === null || searchParams.get("offset") === null) {
-			return;
-		}
+    const limit = parseInt(searchParams.get('limit')!, 10)
+    const offset = parseInt(searchParams.get('offset')!, 10)
 
-		const limit = parseInt(searchParams.get("limit")!, 10);
-		const offset = parseInt(searchParams.get("offset")!, 10);
+    options = { limit, offset }
+    console.log(nextURL)
+    if (nextURL === null) {
+      return
+    }
 
-		options = { limit, offset };
-		console.log(nextURL);
-		if (nextURL === null) {
-			return;
-		}
+    await sleep()
 
-		await sleep();
+    // await fetchTracks(artistName, options, stopCount + 1);
+  } catch (error) {
+    console.error(error)
+  }
+}
 
-		// await fetchTracks(artistName, options, stopCount + 1);
-	} catch (error) {
-		console.error(error);
-	}
-};
+// spotifyApi
+//   .clientCredentialsGrant()
+//   .then((response: any) => {
+//     spotifyApi.setAccessToken(response.body.access_token)
+//     fetchTracks('Davido')
+//   })
+//   .catch((err) => {
+//     console.log(err)
+//   })
 
-spotifyApi
-	.clientCredentialsGrant()
-	.then((response: any) => {
-		spotifyApi.setAccessToken(response.body.access_token);
-		fetchTracks("Davido");
-	})
-	.catch((err) => {
-		console.log(err);
-	});
+const getUserAccessToken = async (): Promise<string> => {
+  const credentialsRes = await spotifyApi.clientCredentialsGrant()
+  const access_token = credentialsRes.body.access_token
 
+  return access_token
+}
 
+export interface ARTIST_TYPE {
+  external_urls: ExternalUrls
+  followers: Followers
+  genres: string[]
+  href: string
+  id: string
+  images: Image[]
+  name: string
+  popularity: number
+  type: string
+  uri: string
+}
+
+export interface ExternalUrls {
+  spotify: string
+}
+
+export interface Followers {
+  href: null
+  total: number
+}
+
+export interface Image {
+  height: number
+  url: string
+  width: number
+}
+
+export async function getArtist(artistName: string): Promise<Array<ARTIST_TYPE> | null> {
+  if (artistName === undefined) {
+    return null
+  }
+
+  const accessToken = await getUserAccessToken()
+  spotifyApi.setAccessToken(accessToken)
+  const artistList = (await spotifyApi.searchArtists(artistName)).body.artists
+    ?.items as Array<ARTIST_TYPE> | null
+
+  return artistList
+}
