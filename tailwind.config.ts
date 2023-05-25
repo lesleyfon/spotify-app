0948d81702fad3ce5777112e20a6d3064233c3e1
lesley <lesleybanadzem2017@gmail.com>
2023-05-24 23:34:57 -0500

    Addedd colors shades for app

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,7 @@
 import type { Config } from 'tailwindcss'
 
 export default {
-  content: ["./app/**/*.{js,jsx,ts,tsx}"],
+  content: ['./app/**/*.{js,jsx,ts,tsx}'],
   prefix: 'tw-',
   theme: {
     container: {
@@ -14,13 +14,26 @@ export default {
       xl: '1440px',
     },
     colors: {
-      'blue': '#1fb6ff',
-      'pink': '#ff49db',
-      'orange': '#ff7849',
-      'green': '#13ce66',
+      blue: '#1fb6ff',
+      pink: '#ff49db',
+      orange: '#ff7849',
+      green: '#13ce66',
       'gray-dark': '#273444',
-      'gray': '#8492a6',
+      gray: '#8492a6',
       'gray-light': '#d3dce6',
+      wedgewood: {
+        '50': '#f2f9f9',
+        '100': '#ddeff0',
+        '200': '#bfe0e2',
+        '300': '#92cace',
+        '400': '#5faab1',
+        '500': '#438e96',
+        '600': '#3b757f',
+        '700': '#356169',
+        '800': '#325158',
+        '900': '#2d464c',
+        '950': '#1a2c32',
+      },
     },
     fontFamily: {
       sans: ['Graphik', 'sans-serif'],
@@ -33,9 +46,8 @@ export default {
       },
       borderRadius: {
         '4xl': '2rem',
-      }
-    }
+      },
+    },
   },
   plugins: [],
 } satisfies Config
-
