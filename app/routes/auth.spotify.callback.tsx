0cf4aafc9cb0a4a17e71f59009ad090236314947
lesley <lesleybanadzem2017@gmail.com>
2023-05-24 23:39:11 -0500

    Added loader for handling the spotify callback.

---
+++ app/routes/auth.spotify.callback.tsx
@@ -0,0 +1,9 @@
+import type { LoaderArgs } from '@remix-run/node'
+import { authenticator } from '~/service/auth.server'
+
+export async function loader({ request }: LoaderArgs) {
+  return authenticator.authenticate('spotify', request, {
+    successRedirect: '/',
+    failureRedirect: '/login',
+  })
+}
