cdcf4445139038b530e6c5888269339886e47469
lesley <lesleybanadzem2017@gmail.com>
2023-05-26 21:24:36 -0500

    Added some more eslint rules

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,4 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from 'tailwindcss';
 
 export default {
   content: ['./app/**/*.{js,jsx,ts,tsx}'],
@@ -50,4 +50,4 @@ export default {
     },
   },
   plugins: [],
-} satisfies Config
+} satisfies Config;
